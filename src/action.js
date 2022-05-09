dd98bab29ba9a69ace1e86bff5d81398c9fef052
suleimanabubakar <suleiman.u.abubakar@gmail.com>
2022-05-09 17:49:49 +0300

    bug fix

--- src/action.js
+++ src/action.js
@@ -3,8 +3,9 @@ const core = require("@actions/core");
 
 
 async function run (){
-    const token  = core.getInput('GITHUB_TOKEN')
-    console.log('token is ', token)
+    // const token  = core.getInput('GITHUB_TOKEN')
+    // console.log('token is ', token)
+    token = 'xysxs'
   
     const project = core.getInput('PROJECT')
     const octokit = github.getOctokit(token);
